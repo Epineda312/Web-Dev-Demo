037550daed4c94d04db91278c1812cbb38affece
Airic <epineda773@protonmail.com>
2019-03-17 02:42:09 -0500

    Update script.js

--- JavaScript/JSquestions/script.js
+++ JavaScript/JSquestions/script.js
@@ -7,8 +7,9 @@ var firstName = prompt("What is your first name?");
 var lastName = prompt("What is your last name?");
 
 //ask user for age
-var age = prompt("How old are you");
+var age = prompt("How old are you");\
 
+//Create a full name variable using user input
 var fullName = firstName + " " + lastName;
 
 //form a sentence using user input
