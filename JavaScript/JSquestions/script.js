8974f546a8c68d47d74694260aa248cd6a11a11b
Airic <epineda773@protonmail.com>
2019-03-17 02:41:17 -0500

    Add files via upload
    JavaScript practice files will be stored here. Included right now is a script that asks the user several simple questions.

---
+++ JavaScript/JSquestions/script.js
@@ -0,0 +1,21 @@
+//JS questions example
+
+//ask user for first name
+var firstName = prompt("What is your first name?");
+
+//ask user for last name
+var lastName = prompt("What is your last name?");
+
+//ask user for age
+var age = prompt("How old are you");
+
+var fullName = firstName + " " + lastName;
+
+//form a sentence using user input
+alert("Nice to meet you, " + firstName + " " + lastName);
+alert("I see you are " + age +" years old")
+
+//print user input to console
+console.log("Your full name is " + fullName);
+console.log("You are " + age +" years old")
+
