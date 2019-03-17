c8bed8ec4090804f6e2346c9de0b7b71abf6f659
Airic <epineda773@protonmail.com>
2019-03-17 02:56:12 -0500

    Add files via upload
    script that asks user for age and determines how many days user has lived

---
+++ JavaScript/ageCalculator/script.js
@@ -0,0 +1,7 @@
+//Script that asks user for age 
+//Uses age to determine days alive
+var age = prompt("How old are you?");
+var daysLived = age * 365;
+
+alert("You have lived for " + daysLived + " days.");
+console.log("You have lived for " + daysLived + " days.");
