8932a0bac8f01a1cc14250fe798f4247a63aefaa
Airic <epineda773@protonmail.com>
2019-03-19 03:45:07 -0500

    Add files via upload

---
+++ JavaScript/Annoy/annoy.js
@@ -0,0 +1,15 @@
+var answer = prompt("are we there yet?");
+
+while(answer.indexOf("yes") === -1){
+	var answer = prompt("are we there yet?");
+
+}
+
+alert("YAY, WE MADE IT!!!!");
+ 
+
+// if(answer == "yes"){
+// 	alert("YAY, We made it!")
+// } else{
+// 	var answer = prompt("are we there yet?");
+// }
